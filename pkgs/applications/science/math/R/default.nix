9cb2eebf61571f1cc13d156750c39f8d78186657
Christopher Villalobos <christopher.villalobos@gmail.com>
2021-03-12 14:01:35 -0500

    Remove --with-cairo when building on darwin

--- pkgs/applications/science/math/R/default.nix
+++ pkgs/applications/science/math/R/default.nix
@@ -50,7 +50,7 @@ stdenv.mkDerivation rec {
       --with-lapack="-L${lapack}/lib -llapack"
       --with-readline
       --with-tcltk --with-tcl-config="${tcl}/lib/tclConfig.sh" --with-tk-config="${tk}/lib/tkConfig.sh"
-      --with-cairo
+      ${lib.optionalString (!stdenv.isDarwin) "--with-cairo"}
       --with-libpng
       --with-jpeglib
       --with-libtiff
